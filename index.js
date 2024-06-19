15245c93e849a3ccaa4465bd7cddf90c428a5174
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-06-19 13:41:37 +0300

    updated

--- index.js
+++ index.js
@@ -5,13 +5,30 @@ import fs from 'node:fs/promises';
 const targetPath = path.resolve('downloads');
 
 const dirDict = {
-  documents: ['pdf', 'doc', 'docx', 'odt', 'rtf', 'wps', 'txt'],
-  images: ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp'],
-  videos: ['mp4', 'avi', 'mov', 'mkv'],
-  audios: ['mp3', 'wav', 'ogg', 'aac'],
-  arhives: ['zip', 'rar', '7z', 'iso'],
+  documents: [
+    '.pdf',
+    '.doc',
+    '.docx',
+    '.xls',
+    '.xlsx',
+    '.ppt',
+    '.pptx',
+    '.odt',
+    '.ods',
+    '.odp',
+    '.odg',
+    '.rtf',
+    '.wps',
+    '.txt',
+  ],
+  images: ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp'],
+  videos: ['.mp4', '.avi', '.mkv', '.mov'],
+  audios: ['.mp3', '.wav', '.ogg', '.aac'],
+  arhives: ['.zip', '.rar', '.7z', '.iso'],
   others: [],
 };
+
+const isExisDir = {};
 
 async function dirHandler(dirPath) {
   const data = await fs.readdir(dirPath);
@@ -24,29 +41,39 @@ async function dirHandler(dirPath) {
   });
   const result = await Promise.allSettled(promisesList);
   const dirStat = result.map(({ value }) => value);
-  //   console.log(dirStat);
 
-  dirStat.forEach((item) => {
+  const finallResult = dirStat.map(async (item) => {
     if (item.isDir) {
-      dirHandler(item.itemPath);
+      await dirHandler(item.itemPath);
     } else {
-      fileHandler(item.itemPath);
+      await fileHandler(item.itemPath);
     }
   });
+  await Promise.allSettled(finallResult);
 }
 
 async function fileHandler(filePath) {
-  const { ext } = path.parse(filePath);
-  console.log(ext);
+  const { ext, base } = path.parse(filePath);
   for (const key in dirDict) {
-    console.log(dirDict[key]);
     if (dirDict[key].includes(ext)) {
-      console.log(key);
+      const sortedDir = path.join(targetPath, key);
+      if (!isExisDir[key]) {
+        isExisDir[key] = true;
+        await fs.mkdir(sortedDir);
+      }
       // write to dir 'key'
+      // await fs.cp(filePath, path.join(sortedDir, base));
+      await fs.rename(filePath, path.join(sortedDir, base));
       return;
     }
   }
   //write to dir 'others'
+  const otherDir = path.join(targetPath, 'others');
+  if (!isExisDir.others) {
+    isExisDir.others = true;
+    await fs.mkdir(otherDir);
+  }
+  await fs.rename(filePath, path.join(otherDir, base));
 }
 
 dirHandler(targetPath);
